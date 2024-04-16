b004358b3282b4471d575530c432c997678bb665
Ciara Dunks <ciara.dunks@rl-institut.de>
2024-04-16 16:38:17 +0200

    Adapted scenario names

--- src/oemof_tabular_plugins/general/pre_processing.py
+++ src/oemof_tabular_plugins/general/pre_processing.py
@@ -57,26 +57,26 @@ def pre_processing(scenario_dir, wacc):
             missing_columns = cost_columns - set(element_df.columns)
 
             # ---------------- POSSIBLE SCENARIO GROUPS FOR PARAMETER ENTRIES ----------------
-            # scenario group a: annuity parameter is included and all of capex, opex fix and lifetime parameters
-            # are not included in the csv file
+            # scenario group "annuity no cost params": annuity parameter is included and all of capex, opex fix
+            # and lifetime parameters are not included in the csv file
             if annuity_cost in element_df.columns and cost_columns == missing_columns:
-                scenario_group = "a"
-            # scenario group b: annuity parameter is included and some but not all of capex, opex fix and lifetime
-            # parameters are included in the csv file
+                scenario_group = "annuity no cost params"
+            # scenario group "annuity partial cost params": annuity parameter is included and some but not all of
+            # capex, opex fix and lifetime parameters are included in the csv file
             elif annuity_cost in element_df.columns and missing_columns and cost_columns != missing_columns:
-                scenario_group = "b"
-            # scenario group c: annuity parameter is included and all of capex, opex fix and lifetime parameters
-            # are included in the csv file
+                scenario_group = "annuity partial cost params"
+            # scenario group "annuity all cost params": annuity parameter is included and all of capex, opex fix
+            # and lifetime parameters are included in the csv file
             elif annuity_cost in element_df.columns and not missing_columns:
-                scenario_group = "c"
-            # scenario group d: annuity parameter is not included and all of capex, opex fix and lifetime parameters
-            # are included in the csv file
-            elif annuity_cost not in element_df.columns:
-                scenario_group = "d"
-            # scenario group e: the annuity parameter is not included and neither are capex, opex fix and lifetime
-            # parameters in the csv file
+                scenario_group = "annuity all cost params"
+            # scenario group "no annuity partial/all cost params": annuity parameter is not included and at least one
+            # of capex, opex fix and lifetime parameters are included in the csv file
             elif annuity_cost not in element_df.columns and cost_columns == missing_columns:
-                scenario_group = "e"
+                scenario_group = "no annuity partial/all cost params"
+            # scenario group "no annuity no cost params": the annuity parameter is not included and neither are
+            # capex, opex fix and lifetime parameters in the csv file
+            elif annuity_cost not in element_df.columns and cost_columns == missing_columns:
+                scenario_group = "no annuity no cost params"
 
             # print statement for testing, remove later
             print("scenario_group: ", scenario_group)
@@ -86,81 +86,82 @@ def pre_processing(scenario_dir, wacc):
             for index, row in element_df.iterrows():
                 # define the row name
                 row_name = row['name']
-                if scenario_group == "a":
-                    # scenario a1: the annuity parameter is left empty and the other cost parameters have not been
-                    # included
+                if scenario_group == "annuity no cost params":
+                    # scenario "annuity empty no cost params": the annuity parameter is left empty and the other cost
+                    # parameters have not been included
                     if pd.isna(row[annuity_cost]):
-                        scenario = "a1"
-                    # scenario a2: the annuity parameter is defined and the other cost parameters have not been
-                    # included
+                        scenario = "annuity empty no cost params"
+                    # scenario "annuity defined no cost params": the annuity parameter is defined and the other cost
+                    # parameters have not been included
                     else:
-                        scenario = "a2"
-                elif scenario_group == "b":
-                    # scenario b1: the annuity parameter is left empty and only some of the other financial
-                    # parameters are included
+                        scenario = "annuity defined no cost params"
+                elif scenario_group == "annuity partial cost params":
+                    # scenario "annuity empty partial cost params": the annuity parameter is left empty and only
+                    # some of the other financial parameters are included
                     if pd.isna(row[annuity_cost]):
-                        scenario = "b1"
-                    # scenario b2: the annuity parameter is defined and only some of the other financial
-                    # parameters are included
+                        scenario = "annuity empty partial cost params"
+                    # scenario "annuity defined partial cost params": the annuity parameter is defined and only
+                    # some of the other financial parameters are included
                     else:
-                        scenario = "b2"
-                elif scenario_group == "c":
+                        scenario = "annuity defined partial cost params"
+                elif scenario_group == "annuity all cost params":
                     # store the parameters
                     capex = row['capex']
                     opex_fix = row['opex_fix']
                     lifetime = row['lifetime']
-                    # scenario c1: the annuity parameter is left empty and all of the other financial parameters
-                    # are defined
+                    # scenario "annuity empty all cost params": the annuity parameter is left empty and all of the
+                    # other financial parameters are defined
                     if pd.isna(row[annuity_cost]) and pd.notna(capex) and pd.notna(opex_fix) and \
                             pd.notna(lifetime):
-                        scenario = "c1"
-                    # scenario c2: the annuity parameter is either defined or empty, but at least one of
-                    # 'capex', 'opex_fix' and 'lifetime' is left empty
+                        scenario = "annuity empty all cost params defined"
+                    # scenario "annuity all cost params some empty": the annuity parameter is either defined or empty,
+                    # but at least one of 'capex', 'opex_fix' and 'lifetime' is left empty
                     elif pd.isna(capex) or pd.isna(opex_fix) or pd.isna(lifetime):
-                        scenario = "c2"
-                    # scenario c3: both the annuity parameter is defined and all of the other financial parameters
-                    # are defined
+                        scenario = "annuity all cost params some empty"
+                    # scenario "annuity defined all cost params defined": both the annuity parameter is defined and
+                    # all of the other financial parameters are defined
                     else:
-                        scenario = "c3"
-                elif scenario_group == "d":
+                        scenario = "annuity defined all cost params defined"
+                elif scenario_group == "no annuity partial/all cost params":
                     # store the parameters
                     capex = row['capex']
                     opex_fix = row['opex_fix']
                     lifetime = row['lifetime']
-                    # scenario d1: at least one of 'capex', 'opex_fix' and 'lifetime' is left empty
+                    # scenario "no annuity partial/all cost params empty": at least one of 'capex', 'opex_fix' and
+                    # 'lifetime' is left empty
                     if pd.isna(capex) or pd.isna(opex_fix) or pd.isna(lifetime):
-                        scenario = "d1"
-                    # scenario d2: all financial parameters are defined
+                        scenario = "no annuity partial/all cost params empty"
+                    # scenario "no annuity all cost params defined": all financial parameters are defined
                     else:
-                        scenario = "d2"
-                elif scenario_group == "e":
-                    # neither the annuity or financial parameters are defined
-                    scenario = "e1"
+                        scenario = "no annuity all cost params defined"
+                elif scenario_group == "no annuity no cost params":
+                    # scenario "no annuity no cost params": neither the annuity or financial parameters are defined
+                    scenario = "no annuity no cost params"
 
                 # ---------------- ACTIONS TAKEN FOR EACH SCENARIO ----------------
-                if scenario == "a1":
+                if scenario == "annuity empty no cost params":
                     # raise value error
                     raise ValueError(f"'{annuity_cost}' (the annuity) has been left empty for '{row_name}' "
                                      f"in '{element}', and 'capex', 'opex_fix' and 'lifetime' have not "
                                      f" been included. \nEither the annuity ('{annuity_cost}') must be "
                                      f"directly stated or all of the other financial parameters must be stated "
                                      f"to calculate the annuity.")
-                elif scenario == "a2":
+                elif scenario == "annuity defined no cost params":
                     # log info message
                     logger.info(f"The annuity cost is directly used for '{row_name}' in '{element}'.")
-                elif scenario == "b1":
+                elif scenario == "annuity empty partial cost params":
                     # raise value error
                     raise ValueError(f"'{annuity_cost}' (the annuity) has been left empty for '{row_name}' "
                                      f"in '{element}', and not all of 'capex', 'opex_fix' and 'lifetime' have"
                                      f" been included. \nEither the annuity ('{annuity_cost}') must be "
                                      f"directly stated or all of the other financial parameters must be stated "
                                      f"to calculate the annuity.")
-                elif scenario == "b2":
+                elif scenario == "annuity defined partial cost params":
                     # raise value error
                     raise ValueError(f"'{annuity_cost}' is defined and some but not all of 'capex', 'opex_fix'"
                                      f"and 'lifetime' have been included. Please either only state '{annuity_cost}'"
                                      f"or include all other financial parameters to calculate the annuity.")
-                elif scenario == "c1":
+                elif scenario == "annuity empty all cost params defined":
                     # calculate the annuity using the calculate_annuity function
                     capacity_cost = calculate_annuity(capex, opex_fix, lifetime, wacc)
                     # update the dataframe
@@ -168,11 +169,11 @@ def pre_processing(scenario_dir, wacc):
                     # log info message
                     logger.info(f"the annuity ('{annuity_cost}') has been calculated and updated for"
                                 f" '{row_name}' in '{element}'.")
-                elif scenario == "c2":
+                elif scenario == "annuity all cost params some empty":
                     # raise value error
                     raise ValueError(f"One or more of 'capex', 'opex_fix' and 'lifetime' have been left "
                                      f"empty. Please enter values or remove the parameters.")
-                elif scenario == "c3":
+                elif scenario == "annuity defined all cost params defined":
                     # if all parameters are defined, the user is asked if they want to calculate the annuity
                     # from the capex, opex_fix and lifetime or use the annuity directly
                     logger.info(f"All parameters ('capex', 'opex_fix', 'lifetime') and '{annuity_cost}' are "
@@ -205,12 +206,12 @@ def pre_processing(scenario_dir, wacc):
                             # if the user enters something other than yes or no, they are asked to re-enter
                             # their answer
                             print("Invalid choice. Please enter 'yes' or 'no'.")
-                elif scenario == "d1":
+                elif scenario == "no annuity partial/all cost params empty":
                     # raise value error
                     raise ValueError(f"One or more of 'capex', 'opex_fix' and 'lifetime' have been left "
                                      f"empty. Please enter values or remove the parameters and include "
                                      f"the 'capacity_cost'.")
-                elif scenario == "d2":
+                elif scenario == "no annuity all cost params defined":
                     # calculate the annuity using the calculate_annuity function
                     capacity_cost = calculate_annuity(capex, opex_fix, lifetime, wacc)
                     # update the dataframe
@@ -218,7 +219,7 @@ def pre_processing(scenario_dir, wacc):
                     # log info message
                     logger.info(f"the annuity ('{annuity_cost}') has been calculated and updated for"
                                 f" '{row_name}' in '{element}'.")
-                elif scenario == "e1":
+                elif scenario == "no annuity no cost params":
                     logger.info(f"'{element}' does not contain '{annuity_cost}' parameter. Skipping...")
 
             # save the updated dataframe to the csv file
