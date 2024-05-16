90a8b2420b40dd320b3c2f6503ab2735e9ae8101
Ciara Dunks <ciara.dunks@rl-institut.de>
2024-05-16 08:34:45 +0200

    First changes to infer.py for wefe_pv_panel

--- examples/scenarios/wefe_pv_panel/scripts/infer.py
+++ examples/scenarios/wefe_pv_panel/scripts/infer.py
@@ -9,13 +9,19 @@ from oemof.tabular.datapackage import building
 if "kwargs" not in locals():
     kwargs = {}
 
+# This automatically generates a datapackage.json file and the foreign keys should be
+# updated depending on the energy system design
 building.infer_metadata(
-    package_name="oemof-tabular-simple-system1",
+    package_name="wefe-pv-panel",
     foreign_keys={
-        "bus": ["volatile", "dispatchable", "storage", "load"],
-        "profile": ["load", "volatile"],
-        "from_to_bus": ["link"],
-        "chp": [],
+        "bus": [
+            "dispatchable",
+            "load",
+            "excess",
+            "pv-panel"
+        ],
+        "profile": ["dispatchable", "load", "pv-panel"],
+        "from_to_bus": ["pv-panel"],
     },
     **kwargs,
 )
