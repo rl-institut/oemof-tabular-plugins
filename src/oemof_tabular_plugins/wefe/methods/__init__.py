4a72532ef09c67ce37b09dd2c56a185e6307d711
Ciara Dunks <ciara.dunks@rl-institut.de>
2024-05-21 12:12:15 +0200

    Adapted the WEFE subpackage structure

---
+++ src/oemof_tabular_plugins/wefe/methods/__init__.py
@@ -0,0 +1,4 @@
+"""
+This subpackage of wefe will contain certain methods for calculating component inputs
+ToDo: decide if this package is necessary or if these methods can be included directly in the facades
+"""
