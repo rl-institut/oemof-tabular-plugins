c661d73ad5e6866f6452d0c8b6f55a403f443c05
Ciara Dunks <ciara.dunks@rl-institut.de>
2024-05-21 12:11:34 +0200

    Adapted the general subpackage structure

--- src/oemof_tabular_plugins/wefe/post_processing/post_processing.py
+++ src/oemof_tabular_plugins/wefe/post_processing/post_processing.py
@@ -1,12 +1,6 @@
-"""
-This file will contain all of the specific post-processing functionalities for water-energy-food-environment (WEFE)
-systems e.g. calculating the crop yield and the total water footprint
-"""
-
-
 def calculate_crop_yield():
     return
 
 
-def calculate_total_water_footprint():
+def calculate_water_dissiptated():
     return
