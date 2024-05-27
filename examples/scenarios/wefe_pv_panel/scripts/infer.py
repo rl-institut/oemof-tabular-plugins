15839777d5aed5ce490154b6ad5a2a6a972b3013
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2024-05-27 15:21:49 +0200

    Add code which is meant to be in oemof.tabular later

--- examples/scenarios/wefe_pv_panel/scripts/infer.py
+++ examples/scenarios/wefe_pv_panel/scripts/infer.py
@@ -3,7 +3,7 @@ Run this script from the root directory of the datapackage to update or create m
 form of a json file (datapackage.json)
 """
 
-from oemof.tabular.datapackage import building
+# TODO this should be with from oemof.tabular.datapackage import building when https://github.com/oemof/oemof-tabular/pull/173 is merged
 from oemof_tabular_plugins.datapackage import building as otp_building
 
 # this part is for testing only: It allows to pass
@@ -11,28 +11,7 @@ from oemof_tabular_plugins.datapackage import building as otp_building
 if "kwargs" not in locals():
     kwargs = {}
 
-# this automatically generates a datapackage.json file and the foreign keys should be
-# updated depending on the energy system design (works for existing facades, but for
-# new facades the additional 'add_foreign_keys_to_datapackage' function might be needed)
-building.infer_metadata(
+# this automatically generates a datapackage.json file
+otp_building.infer_metadata_from_data(
     package_name="wefe-pv-panel",
-    foreign_keys={
-        "bus": ["dispatchable", "load", "excess", "pv-panel"],
-        "profile": ["dispatchable", "load", "pv-panel"],
-        "from_to_bus": ["pv-panel"],
-    },
-    **kwargs,
-)
-
-# this function adds any additional foreign keys to the datapackage.json file
-otp_building.add_foreign_keys_to_datapackage(
-    "datapackage.json",
-    {
-        "pv_panel": {
-            "from_bus": {"resource": "bus", "fields": "name"},
-            "to_bus": {"resource": "bus", "fields": "name"},
-            "t_air": {"resource": "pv_panel_profile"},
-            "ghi": {"resource": "pv_panel_profile"},
-        }
-    },
 )
