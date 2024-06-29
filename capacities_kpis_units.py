a50cc1036e0a6ab860d0ea92ec7ce29d2a712f04
Julian Fleischmann <julianfleischmann@gmail.com>
2024-06-29 19:20:48 +0200

    Add dict containing Units of Capacities and Kpis in Results

---
+++ capacities_kpis_units.py
@@ -0,0 +1,5 @@
+#  Units of Capacities and Kpis in Results
+capacities_units = {'battery_storage': '[kWh]', 'inverter': '[kW]', 'pv-panel': '[kW]', 'water-storage': '[m³]',
+                    'mimo': '[m³/h]'}
+kpis_units = {'annuity_total': '[$]', 'total_upfront_investments': '[$]', 'land_requirement_total': '[m²]',
+              'total_water_footprint': '[m³]'}
