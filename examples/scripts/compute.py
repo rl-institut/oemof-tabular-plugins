76450b0b0a7a9b93f8a604c4ab1c09483b7ada47
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2024-08-22 14:18:04 +0200

    Move compute single scenario to a function
    This is necessary to run tests

--- examples/scripts/compute.py
+++ examples/scripts/compute.py
@@ -4,19 +4,13 @@ from oemof.solph import processing
 from oemof.solph.processing import parameter_as_dict
 
 # TODO this should be with from oemof.tabular.datapackage import building when https://github.com/oemof/oemof-tabular/pull/173 is merged
-from oemof_tabular_plugins.datapackage import building as otp_building
+from oemof_tabular_plugins.script import compute_scenario
 
 # ---- imports to be used when the package has been installed ----
 from oemof.tabular import datapackage  # noqa
 from oemof.tabular.facades import TYPEMAP
 
-# ---- imports from oemof-tabular-plugins package ----
-from oemof_tabular_plugins.general import (
-    post_processing,
-    CONSTRAINT_TYPE_MAP,
-    pre_processing,
-    logger,
-)
+
 from oemof_tabular_plugins.wefe.facades import PVPanel, MIMO
 
 
@@ -34,6 +28,8 @@ scenarios = [
     "wefe_custom_attributes",
     "wefe_pv_panel",
     "wefe_reverse_osmosis",
+    # "carriers_in_busses_only",
+    # "carriers_over_defined",
 ]
 # weighted average cost of capital (WACC) - might move later
 # this parameter is needed if CAPEX, OPEX fix and lifetime are included
@@ -60,56 +56,16 @@ for scenario in scenarios:
     # set paths for scenario and result directories
     scenario_dir = os.path.join(project_dir, "scenarios", scenario)
     results_path = os.path.join(project_dir, "results", scenario, "output")
-    # create results directory if it doesn't already exist
-    if not os.path.exists(results_path):
-        os.makedirs(results_path)
 
-    # pre-processing to update input csv files based on cost parameters: CAPEX, OPEX fix, lifetime, WACC
-    pre_processing(scenario_dir, wacc, custom_attributes, moo)
-
-    otp_building.infer_metadata_from_data(
-        package_name=scenario,
-        path=scenario_dir,
-        typemap=TYPEMAP,
-    )
-
-    # create energy system object from the datapackage
-    es = EnergySystem.from_datapackage(
-        os.path.join(scenario_dir, "datapackage.json"),
-        attributemap={},
-        typemap=TYPEMAP,
-    )
-
-    logger.info("Energy system created from datapackage")
-
-    # create model from energy system (this is just oemof.solph)
-    m = Model(es)
-    logger.info("Model created from energy system")
-
-    # add constraints from datapackage to the model
-    m.add_constraints_from_datapackage(
-        os.path.join(scenario_dir, "datapackage.json"),
-        constraint_type_map=CONSTRAINT_TYPE_MAP,
-    )
-    logger.info("Constraints added to model")
-
-    # if you want dual variables / shadow prices uncomment line below
-    # m.receive_duals()
-
-    # select solver 'gurobi', 'cplex', 'glpk' etc
-    m.solve("cbc")
-
-    # extract parameters and results
-    params = parameter_as_dict(es)
-    es.results = processing.results(m)
-
-    post_processing(
-        params,
-        es,
+    compute_scenario(
+        scenario_dir,
         results_path,
-        dp_path=os.path.join(scenario_dir, "datapackage.json"),
-        dash_app=True,
-        parameters_units=None,
+        wacc,
+        scenario_name=scenario,
+        custom_attributes=custom_attributes,
+        typemap=TYPEMAP,
+        moo=moo,
     )
+
 
 print("done")
---
+++ src/oemof_tabular_plugins/script/compute.py
@@ -0,0 +1,111 @@
+import os
+from oemof.solph import EnergySystem, Model
+from oemof.solph import processing
+from oemof.solph.processing import parameter_as_dict
+
+# TODO this should be with from oemof.tabular.datapackage import building when https://github.com/oemof/oemof-tabular/pull/173 is merged
+from oemof_tabular_plugins.datapackage import building as otp_building
+
+# ---- imports to be used when the package has been installed ----
+from oemof.tabular import datapackage  # noqa
+from oemof.tabular.facades import TYPEMAP
+
+# ---- imports from oemof-tabular-plugins package ----
+from oemof_tabular_plugins.general import (
+    post_processing,
+    CONSTRAINT_TYPE_MAP,
+    pre_processing,
+    logger,
+)
+
+
+def compute_scenario(
+    scenario_dir,
+    results_path,
+    wacc,
+    scenario_name=None,
+    custom_attributes=None,
+    typemap=None,
+    moo=False,
+):
+    """
+
+    Parameters
+    ----------
+    scenario_dir
+    results_path
+    wacc
+    scenario_name
+    custom_attributes
+    typemap: default to oemof.tabular.facades.TYPEMAP
+    moo
+
+    Returns
+    -------
+
+    """
+    if scenario_name is None:
+        scenario = os.path.dirname(scenario_dir)
+
+    if custom_attributes is None:
+        custom_attributes = [
+            "emission_factor",
+            "renewable_factor",
+            "land_requirement_factor",
+            "water_footprint_factor",
+        ]
+
+    if typemap is None:
+        typemap = TYPEMAP
+
+    # create results directory if it doesn't already exist
+    if not os.path.exists(results_path):
+        os.makedirs(results_path)
+
+    # pre-processing to update input csv files based on cost parameters: CAPEX, OPEX fix, lifetime, WACC
+    pre_processing(scenario_dir, wacc, custom_attributes, moo)
+
+    otp_building.infer_metadata_from_data(
+        package_name=scenario_name,
+        path=scenario_dir,
+        typemap=typemap,
+    )
+
+    # create energy system object from the datapackage
+    es = EnergySystem.from_datapackage(
+        os.path.join(scenario_dir, "datapackage.json"),
+        attributemap={},
+        typemap=typemap,
+    )
+
+    logger.info("Energy system created from datapackage")
+
+    # create model from energy system (this is just oemof.solph)
+    m = Model(es)
+    logger.info("Model created from energy system")
+
+    # add constraints from datapackage to the model
+    m.add_constraints_from_datapackage(
+        os.path.join(scenario_dir, "datapackage.json"),
+        constraint_type_map=CONSTRAINT_TYPE_MAP,
+    )
+    logger.info("Constraints added to model")
+
+    # if you want dual variables / shadow prices uncomment line below
+    # m.receive_duals()
+
+    # select solver 'gurobi', 'cplex', 'glpk' etc
+    m.solve("cbc")
+
+    # extract parameters and results
+    params = parameter_as_dict(es)
+    es.results = processing.results(m)
+
+    post_processing(
+        params,
+        es,
+        results_path,
+        dp_path=os.path.join(scenario_dir, "datapackage.json"),
+        dash_app=True,
+        parameters_units=None,
+    )
