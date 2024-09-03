229b279c21edd6be8b80babd0a2d363fa35b2d20
Max Libberoth <Max.Libberoth@rl-institut.de>
2024-09-03 14:36:43 +0200

    Updated crop_dict and soil_dict including all crops from SIMPLE

--- src/oemof_tabular_plugins/wefe/global_specs/soil.py
+++ src/oemof_tabular_plugins/wefe/global_specs/soil.py
@@ -1,3 +1,39 @@
+"""
+Source
+------------
+SIMPLE crop model: Site information and related soil parameters (Table 1b)
+https://doi.org/10.1016/j.eja.2019.01.009
+
+If multiple values for different cultivars are given, the average is used.
+
+Please note that these values are stored as global parameters for every crop,
+even though they reflect specific locations which may lead to errors.
+
+Crops
+------------
+wheat
+rice
+maize
+soybean
+drybean
+peanut
+potato
+cassava
+tomato
+sweetcorn
+greenbean
+carrot
+cotton
+banana
+
+Parameters
+------------
+awc:    average water holding capacity [-]
+rcn:    runoff curve number [-]
+ddc:    deep drainage coefficient [-]
+rzd:    root zone depth [mm]
+"""
+
 soil_dict = {
     'banana': {
         'awc': 0.11,
@@ -22,5 +58,59 @@ soil_dict = {
         'rcn': 75.5,
         'ddc': 0.3,
         'rzd': 900,
+    },
+    'rice': {
+        'awc': 0.12,
+        'rcn': 70,
+        'ddc': 0.3,
+        'rzd': 400,
+    },
+    'maize': {
+        'awc': 0.12,
+        'rcn': 70,
+        'ddc': 0.3,
+        'rzd': 1500,
+    },
+    'soybean': {
+        'awc': 0.13,
+        'rcn': 70,
+        'ddc': 0.57,
+        'rzd': 1630,
+    },
+    'drybean': {
+        'awc': 0.08,
+        'rcn': 65,
+        'ddc': 0.5,
+        'rzd': 700,
+    },
+    'potato': {
+        'awc': 0.11,
+        'rcn': 67.25,
+        'ddc': 0.625,
+        'rzd': 825,
+    },
+    'sweetcorn': {
+        'awc': 0.25,
+        'rcn': 60,
+        'ddc': 0.7,
+        'rzd': 1500,
+    },
+    'green bean': {
+        'awc': 0.08,
+        'rcn': 65,
+        'ddc': 0.5,
+        'rzd': 1000,
+    },
+    'carrot': {
+        'awc': 0.09,
+        'rcn': 75,
+        'ddc': 0.4,
+        'rzd': 500,
+    },
+    'cotton': {
+        'awc': 0.12,
+        'rcn': 70,
+        'ddc': 0.3,
+        'rzd': 600,
     }
 }
