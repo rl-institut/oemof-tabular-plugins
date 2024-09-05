22b95dd32f9b1ab1c3e5436c8b7fc6a1bede1f91
JulianBarinton <julianfleischmann@gmail.com>
2024-09-05 17:01:26 +0200

    Case Studies Colombia final definition of csv elements

--- examples/scripts/compute.py
+++ examples/scripts/compute.py
@@ -26,8 +26,8 @@ scenarios = [
     # "wefe_custom_attributes",
     # "wefe_pv_panel",
     # "wefe_reverse_osmosis",
-    # "aiwa"
-      "arusi"
+      "aiwa"
+    # "arusi"
 ]
 # weighted average cost of capital (WACC) - might move later
 # this parameter is needed if CAPEX, OPEX fix and lifetime are included
