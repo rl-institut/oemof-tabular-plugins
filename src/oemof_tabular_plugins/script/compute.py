92a0db67798a1971c8ce61b946aef52a37ad9987
Vivek-H-Rana <vivekhrana24@gmail.com>
2025-10-08 16:33:58 +0200

    Fix oemof.visio import in compute.py

--- src/oemof_tabular_plugins/script/compute.py
+++ src/oemof_tabular_plugins/script/compute.py
@@ -7,7 +7,7 @@ from oemof.solph.processing import parameter_as_dict
 
 
 try:
-    from oemof_visio import ESGraphRenderer
+    from oemof.visio import ESGraphRenderer
 
     ES_GRAPH = True
 except ModuleNotFoundError:
