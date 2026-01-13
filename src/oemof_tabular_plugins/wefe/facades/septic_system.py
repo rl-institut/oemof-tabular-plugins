9448939ff6de39aeebe9ce60f2ebd0c2538ecd18
Vivek-H-Rana <vivekhrana24@gmail.com>
2026-01-13 06:00:52 +0100

    Add Septic System, Constructed Wetland, Centralized WWTP, Decentralized WWTP and Water Reuse System in v2.0 (MIMO) facades

--- src/oemof_tabular_plugins/wefe/facades/septic_system.py
+++ src/oemof_tabular_plugins/wefe/facades/septic_system.py
@@ -33,7 +33,7 @@ class SepticSystem(Converter, Facade):
         Specific electricity demand/consumption in kWh per m³ treated water. Default: 0.13 (~0.0 ,if assumed gravity based)
     efficiency: float
         Share of untreated water that becomes treated water.
-        Value between 0 and 1. Default: 0.80
+        Value between 0 and 1. Default: 0.70
     capacity: numeric
         The water treatment capacity (output side) of the unit.
     carrier_cost: numeric
